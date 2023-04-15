59a4d8839de745c1c7b20c5120410f5ea7eee997
904day <75007741+904day@users.noreply.github.com>
2023-04-15 23:06:15 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,13 @@
+# 백준 2748 피보나치 수 2
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+dp = [0] * (91)
+dp[1] = 1
+dp[2] = 1
+
+for i in range(3,n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
