0b0bd0abb9c8bc53e72ab5a1d9c2fd473838726a
904day <75007741+904day@users.noreply.github.com>
2023-04-23 17:49:17 +0900

    [Silver V] Title: 날짜 계산, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,14 @@
+# 백준 1476 날짜 계산
+import sys
+read = sys.stdin.readline
+
+E,S,M = map(int,read().split())
+answer = 1
+
+while True:
+    if (answer - E) % 15 == 0 and (answer - S) % 28 == 0 and (answer - M) % 19 == 0:
+        break
+
+    answer += 1
+
+print(answer)
