740d78031e55433ade1a36785badc622099d0f8c
904day <75007741+904day@users.noreply.github.com>
2023-04-30 19:28:31 +0900

    [Bronze II] Title: 진법 변환, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -0,0 +1,12 @@
+N, B = input().split()
+B = int(B)
+result = 0
+
+for i, j in enumerate(N):
+    try:
+        if int(j):
+            result += int(j) * B ** (len(N)-i-1)
+    except:
+        result += (ord(j)-55) * B ** (len(N)-i-1)
+        
+print(result)
