e5666d621191a9e2b5d7ec0b8cda18aac43bdfd2
904day <75007741+904day@users.noreply.github.com>
2023-05-06 23:09:33 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 112 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,32 +1,31 @@
-# 백준 11053 가장 긴 증가하는 수열(Longest Increasing Subsequence)
+# 백준 14002 가장 긴 증가하는 부분수열 4 (Longest Increasing Subsequence)
 import sys
 read = sys.stdin.readline
 
 n = int(read().rstrip())
 a = list(map(int,read().rstrip().split()))
 
+def dfs(x):
+    if x == -1:
+        return
+    
+    dfs(path[x])
+    print(a[x], end = ' ')
+
 # n[i]를 마지막 값으로 가지는 가장 긴 증가부분수열의 길이를 담아 놓을 리스트
-LIS = [ 1 for _ in range(n) ]
+dp = [ 1 for _ in range(n) ]
 
-for i in range(1,n):
-    for j in range(i):
-        if a[i] > a[j]:
-            LIS[i] = max(LIS[i], LIS[j]+1)
+# 역추적을 위한 경로 저장
+# path[i] : dp[i] 최댓값이 갱신 될 당시 어느 인덱스로 부터 왔는지? 
+path = [ -1 for _ in range(n) ]
 
+for current in range(1, n):
+    for prev in range(current):
+        if a[prev] < a[current] and dp[current] < dp[prev] + 1:
+            dp[current] = dp[prev] + 1
+            path[current] = prev
 
-# 역 추적 시작
-# 가장 작은 것부터 추적하면 안 되는 이유는 
-# 만약 5 4 3 2 1 2 3 라는 숫자를 입력받았을 경우 dp는 1 1 1 1 1 2 3 이 된다.
-# 즉, 답이 5 2 3 으로 출력될 수도 있다는 얘기이다.
-
-list = []
-result = max(LIS)
-
-for i in range(n-1,-1,-1): # 거꾸로 탐색 하면서
-    if LIS[i] == result:
-        list.append(a[i])
-        result -= 1
-
-list.reverse()
-print(max(LIS))
-print(*list,sep=' ')        
+ans = max(dp)
+idx = dp.index(ans)
+print(ans)
+dfs(idx)
