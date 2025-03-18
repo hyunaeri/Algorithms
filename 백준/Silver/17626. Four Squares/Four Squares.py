55a0b8c36c288c0a074c208a31146560ff784825
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-03-18 16:24:48 +0900

    [Silver III] Title: Four Squares, Time: 208 ms, Memory: 119284 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,10 +1,12 @@
+# 입력값 n을 받음 (n을 제곱수들의 합으로 표현하는 최소 개수를 구하는 문제)
 import sys
+read = sys.stdin.readline
 
-n = int(sys.stdin.readline().strip())
+n = int(read())
 
 # dp[i]: i개의 제곱수로 만들 수 있는 숫자 리스트
 dp = [set() for _ in range(5)]
-dp[0].add(0)  # 0은 아무것도 더하지 않는 경우
+dp[0].add(0)
 
 # n 이하의 모든 제곱수를 구함
 squares = []
@@ -13,14 +15,15 @@ while i * i <= n:
     squares.append(i * i)
     i += 1
 
-# DP 진행
-for count in range(1, 5):  # 최대 4개의 제곱수까지 가능
-    for num in dp[count - 1]:  # 이전 단계에서 만든 숫자들 확인
+# DP 테이블 채우기
+for count in range(1, 5):
+    for num in dp[count - 1]:
         for square in squares:
             new_num = num + square
             if new_num > n:  # 초과하면 스킵
                 break
             dp[count].add(new_num)
-    if n in dp[count]:  # n이 만들어지면 종료
+            
+    if n in dp[count]:
         print(count)
-        sys.exit()
+        break
