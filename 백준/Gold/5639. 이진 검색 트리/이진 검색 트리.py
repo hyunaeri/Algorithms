ac476dd4cbc8edbe3f7017f2215c649d8203cb45
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-03-25 19:12:01 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 1740 ms, Memory: 35464 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -2,51 +2,41 @@ import sys
 sys.setrecursionlimit(10**5)
 read = sys.stdin.readline
 
-class Node:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
-
-class Tree:
-    def __init__(self):
-        self.root = None
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
     
     def insert(self, data):
-        if self.root is None:
-          self.root = Node(data)
-          
-        else:
-          current_node = self.root
-          while True:
+        current_node = self
+        while True:
             if data < current_node.value:
-              if current_node.left is None:
-                current_node.left = Node(data)
-                break
-              else:
-                current_node = current_node.left
+                if current_node.left is None:
+                    current_node.left = BinaryTree(data)
+                    break
+                else:
+                    current_node = current_node.left
             else:
-              if current_node.right is None:
-                current_node.right = Node(data)
-                break
-              else:
-                current_node = current_node.right
+                if current_node.right is None:
+                    current_node.right = BinaryTree(data)
+                    break
+                else:
+                    current_node = current_node.right
 
-    def printPostOrder(self, node = None):
-      if node is None:
-        node = self.root
-      if node.left is not None:
-          self.printPostOrder(node.left)
-      if node.right is not None:
-          self.printPostOrder(node.right)
-      print(node.value)
+    def printPostOrder(self):
+        if self.left is not None:
+            self.left.printPostOrder()
+        if self.right is not None:
+            self.right.printPostOrder()
+        print(self.value)
 
-tree = Tree()
+tree = BinaryTree(int(read()))
 
 while True:
     try:
         tree.insert(int(read()))
     except:
         break
-
+      
 tree.printPostOrder()
