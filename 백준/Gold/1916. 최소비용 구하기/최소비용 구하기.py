cebcc7785b36691151794c2ff913adee98d58fca
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-03-27 00:33:53 +0900

    [Gold V] Title: 최소비용 구하기, Time: 264 ms, Memory: 58232 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,41 +1,38 @@
-# 백준 1916 최소비용구하기
-import sys
+# 백준 1916 최소비용 구하기
+# 골드 5
+
 import heapq
+import sys
 read = sys.stdin.readline
-INF = int(1e9)
 
-# 도시의 개수, 버스의 개수
-n = int(read())
-m = int(read())
-data = [ [] for _ in range(n+1) ]
+def dijkstra(startPoint, priorityQueue, distance):
+  distance[startPoint] = 0
+  heapq.heappush(priorityQueue, (0, startPoint))
+  
+  while priorityQueue:
+    cost, currentPoint = heapq.heappop(priorityQueue)
+    
+    if distance[currentPoint] < cost:
+      continue
+    
+    for nextPoint, nextCost in bus[currentPoint]:
+      totalCost = cost + nextCost
+      if totalCost < distance[nextPoint]:
+        distance[nextPoint] = totalCost
+        heapq.heappush(priorityQueue, (totalCost, nextPoint))
 
-# 버스 정보 받아오기
-for _ in range(m):
-    u,v,w = map(int, read().rstrip().split())
-    data[u].append((v,w))
+N = int(read())
+M = int(read())
 
-# 출발점, 도착점
-start, end = map(int,read().rstrip().split())
+bus = [ [] for _ in range(N+1) ]
 
-# 최단 거리 테이블
-distance = [INF] * (n+1)
+for _ in range(M):
+  start, end, cost = map(int, read().split())
+  bus[start].append((end, cost))
 
-heap = []
-def dijkstra(start):
-    # 자기자신으로 가는 비용은 0
-    distance[start] = 0
-    heapq.heappush(heap, (0,start))
+startPoint, endPoint = map(int, read().split())
+distance = [float('inf')] * (N+1)
+priorityQueue = []
 
-    while heap:
-        dist, next = heapq.heappop(heap)
-        if distance[next] < dist:
-            continue
-
-        for next_node, weight in data[next]:
-            cost = dist + weight
-            if cost < distance[next_node]:
-                distance[next_node] = cost
-                heapq.heappush(heap, (cost,next_node))
-
-dijkstra(start)
-print(distance[end])
+dijkstra(startPoint, priorityQueue, distance)
+print(distance[endPoint])
