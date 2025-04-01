21677cf7d212827c255f23f796d2a09a67a38653
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-04-01 15:10:39 +0900

    [Silver I] Title: 안전 영역, Time: 656 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,44 +1,45 @@
+# 2025.04.01 (화)
 # 백준 2468 안전 영역
-# DFS
+# 실버 1
 
+from collections import deque
 import sys
-sys.setrecursionlimit(10**5)
 read = sys.stdin.readline
 
-N = int(read().rstrip())
-region = [ list(map(int,read().rstrip().split())) for _ in range(N) ]
-max_height = 0
-answer = 0
+def BFS(x, y, visited, limit):
+  q = deque([(x, y)])
+  visited[x][y] = True
+  
+  while q:
+    x, y = q.popleft()
+    
+    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+      nx, ny = x + dx, y + dy
+      
+      if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
+        if region[nx][ny] > limit:
+          visited[nx][ny] = True
+          q.append((nx, ny))
+
+N = int(read())
+region = [ list(map(int, read().split())) for _ in range(N) ]
+
+maxHeight = 0
 for r in region:
-    max_height = max(max_height, max(r))
+  maxHeight = max(maxHeight, max(r))
 
+answer = 0
 
-# print(region)
-
-def DFS(x,y,limit):
-    visited[x][y] = True
-
-    for dx,dy in [ (-1,0), (1,0), (0,-1), (0,1) ]:
-        nx,ny = x + dx, y + dy
-
-        # 그래프의 범위를 벗어남
-        if nx < 0 or ny < 0 or nx >= N or ny >= N:
-            continue
-
-        if region[nx][ny] > limit and visited[nx][ny] == False :
-            visited[nx][ny] = True
-            DFS(nx,ny,limit)
-
-for k in range(max_height):
-    visited = [ [False]*N for _ in range(N) ]
-    cnt = 0
-
-    for i in range(N):
-        for j in range(N):
-            if region[i][j] > k and visited[i][j] == False :
-                cnt += 1
-                DFS(i,j,k)
-
-    answer = max(cnt, answer)
-
+for h in range(maxHeight):
+  safeArea = 0
+  visited = [ [False] * N for _ in range(N) ]
+  
+  for i in range(N):
+    for j in range(N):
+      if region[i][j] > h and not visited[i][j]:
+        BFS(i, j, visited, h)
+        safeArea += 1
+        
+  answer = max(answer, safeArea)
+  
 print(answer)
