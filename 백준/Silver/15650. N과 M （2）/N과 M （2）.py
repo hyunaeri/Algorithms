6a53773ac1170c8da341d8286173dbf898feee64
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-05-14 15:23:24 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,26 +1,26 @@
-# 백준 15650 N과 M 2
+# 2025.05.14 (수)
+# 백준 15650 N과 M (2)
+# 실버 3
+
 import sys
 read = sys.stdin.readline
 
-# 1~N 까지의 자연수 중 중복없이 M개를 선택한 수열
-# 수열은 오름차순이어야 된다.
-N,M = map(int,read().split())
+# 1 ~ N 까지의 자연수 중에서 중복 없이 M개를 고른 수열 (오름차순)
+N, M = map(int, read().split())
+visited = [False] * (N + 1)
+result = []
 
-visited = [False for _ in range(N+1)] 
-result = [0]
-
-def DFS():
-    # 종료 조건
-    if len(result) == M+1:
-        print(*result[1:], sep = ' ')
-        return
+def backtracking(start):
+  if len(result) == M:
+    print(' '.join(map(str, result)))
+    return
+  
+  for i in range(start, N + 1):
+    if not visited[i]:
+      visited[i] = True
+      result.append(i)
+      backtracking(i + 1)
+      result.pop()
+      visited[i] = False
     
-    for i in range(1, N+1):
-        if not visited[i] and result[-1] < i:
-            visited[i] = True
-            result.append(i)
-            DFS()
-            result.pop()
-            visited[i] = False
-
-DFS()
+backtracking(1)
