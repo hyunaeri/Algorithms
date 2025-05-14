be391b2b386f78d2eb2216a41c2213b0e0b1e789
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-05-14 15:58:00 +0900

    [Silver II] Title: 부분수열의 합, Time: 212 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,28 +1,26 @@
-# 2025.05.14 (수)
-# 백준 1182 부분수열의 합
-# 실버 2
-
 import sys
 read = sys.stdin.readline
 
-# 정수의 개수, 부분수열의 합
 N, S = map(int, read().split())
-
-# 수열, 부분 수열
 arr = list(map(int, read().split()))
-partial_arr = []
 answer = 0
 
-def backtracking(start):
-  global answer
-  if len(partial_arr) > 0 and sum(partial_arr) == S:
-    answer += 1
-  
-  for i in range(start, len(arr)):
-    partial_arr.append(arr[i])
-    backtracking(i + 1)
-    partial_arr.pop()
-    
-backtracking(0)
+def backtracking(start, total):
+    global answer
+    if start == N:
+        if total == S:
+            answer += 1
+        return
+
+    # 현재 원소를 선택하는 경우
+    backtracking(start + 1, total + arr[start])
+    # 현재 원소를 선택하지 않는 경우
+    backtracking(start + 1, total)
+
+backtracking(0, 0)
+
+# 공집합 제외
+if S == 0:
+    answer -= 1
 
 print(answer)
