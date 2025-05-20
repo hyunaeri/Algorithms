77485b7ed939c2244734932d230c8397754efe85
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-05-20 15:54:36 +0900

    [Silver IV] Title: 수 찾기, Time: 484 ms, Memory: 48620 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,30 +1,12 @@
-# 백준 1920 수 찾기
-# 
-import sys
-read = sys.stdin.readline
-
-N = int(read())
-nums = list(map(int,read().split()))
-nums = sorted(nums)
-
-# 타겟
-M = int(read())
-target = list(map(int,read().split()))
-
-def binary_search(target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-
-        if nums[mid] == target:
-            return 1
-
-        elif nums[mid] > target:
-            end = mid - 1
-        
-        elif nums[mid] < target:
-            start = mid + 1
-
-    return 0
-
-for t in target:
-    print(binary_search(t, 0, len(nums)-1))
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
