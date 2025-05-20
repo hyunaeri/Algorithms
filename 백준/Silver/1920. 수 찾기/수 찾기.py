52fca4604c8c1f8245a5caa21cdeb755df38719c
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-05-20 15:59:41 +0900

    [Silver IV] Title: 수 찾기, Time: 460 ms, Memory: 48612 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,12 +1,34 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+# 2025.05.20 (화)
+# 백준 1920 수 찾기
+# 실버 4
+
+import sys
+read = sys.stdin.readline
+
+N = int(read())
+arr = list(map(int, read().split()))
+arr = sorted(arr)
+
+M = int(read())
+check = list(map(int, read().split()))
+
+def binary_search(arr, target):
+  start, end = 0, len(arr) - 1
+  
+  while start <= end:
+    mid = (start + end) // 2
+    
+    if arr[mid] == target:
+      return 1
+      
+    elif arr[mid] < target:
+      start = mid + 1
+      
+    elif arr[mid] > target:
+      end = mid - 1
+  
+  return 0
+
+for num in check:
+  print(binary_search(arr, num))
+
