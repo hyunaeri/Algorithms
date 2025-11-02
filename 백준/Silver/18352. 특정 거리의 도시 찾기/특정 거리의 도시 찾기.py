c3b3989ff0bacfee0dae4911b4f37b29b8e3d7c4
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-11-02 17:28:34 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1508 ms, Memory: 100836 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -10,31 +10,33 @@ read = sys.stdin.readline
 N, M, K, X = map(int, read().rstrip().split())
 
 road = [ [] for _ in range(N+1) ]
-visited = [False] * (N+1)
+dist = [-1] * (N+1)
 
 for _ in range(M):
   src, dst = map(int, read().rstrip().split())
   road[src].append(dst)
-  
-answer = []
 
 def bfs(start):
-  q = deque([(start, 0)])
-  visited[start] = True
+  q = deque([start])
+  dist[start] = 0
   
   while q:
-    current_node, dist = q.popleft()
+    current_node = q.popleft()
     
-    if dist == K:
-      answer.append(current_node)
+    if dist[current_node] == K:
+      continue
     
     for next_node in road[current_node]:
-      if not visited[next_node]:
-        q.append((next_node, dist + 1))
-        visited[next_node] = True
+      if dist[next_node] == -1:
+        q.append((next_node))
+        dist[next_node] = dist[current_node] + 1
 
 bfs(X)
-answer = sorted(answer)    
+
+answer = []
+for i in range(1, N+1):
+  if dist[i] == K:
+    answer.append(i)  
 
 if len(answer) == 0: print(-1)
 else: print(*answer)
