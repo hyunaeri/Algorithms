388dca404d034882db432cd0ac391011a40d6ef3
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-11-04 20:51:37 +0900

    [Gold III] Title: 아기 상어, Time: 128 ms, Memory: 35044 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,74 +1,75 @@
-# 2025.04.03 (목)
-# 백준 16236 아기 상어
+# 백준 16236
 # 골드 3
+# https://www.acmicpc.net/problem/16236
 
 from collections import deque
 import sys
 read = sys.stdin.readline
 
-# 공간의 크기 N*N / 공간의 상태
+# 공간의 크기 N * N
 N = int(read())
-space = [ list(map(int, read().split())) for _ in range(N) ]
-answer = 0
 
-def bfs(x, y, baby_size):
-  can_eat_fish_list = []
-  visited = [ [False] * N for _ in range(N) ]
-  q = deque([(0, x, y)])
-  visited[x][y] = True
-  
-  while q:
-    dist, x, y = q.popleft()
-    
-    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-      nx, ny = x + dx, y + dy
-      
-      # 공간의 범위를 벗어나지 않고, 미 방문한 곳이라면
-      if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
-        # 빈칸이거나 아기 상어와 크기가 같거나 작은 물고기 방향으로만 이동 가능
-        if space[nx][ny] <= baby_size:
-          q.append((dist + 1, nx, ny))
-          visited[nx][ny] = True
-          
-          # 빈칸이 아니고, 아기 상어보다 작은 물고기만 먹을 수 있음
-          if 0 < space[nx][ny] < baby_size:
-            can_eat_fish_list.append((dist + 1, nx, ny))
-  
-  return can_eat_fish_list
+# 공간의 상태
+space = [ list(map(int, read().rstrip().split())) for _ in range(N) ]
 
-# 초기 아기 상어의 위치, 크기
-baby_x, baby_y, baby_size = 0, 0, 2
+# 아기 상어 초기 좌표, 초기 크기
+baby_x, baby_y, baby_size = -1, -1, 2
 
 for i in range(N):
   for j in range(N):
     if space[i][j] == 9:
       baby_x, baby_y = i, j
       space[i][j] = 0
-      break
 
-# 먹방 시작
-eating_count = 0
-
-while True:
-  can_eat_fish_list = bfs(baby_x, baby_y, baby_size)
+def get_possible_fish_list(start_x, start_y, shark_size):
+  possible = []
+  visited = [ [False] * N for _ in range(N) ]
   
-  # 먹을 수 있는 물고기가 없다면 종료
-  if not can_eat_fish_list:
-    break
+  q = deque([(0, start_x, start_y)])
+  visited[start_x][start_y] = True
   
-  # 먹을 수 있는 물고기 중 가장 가까운 물고기 찾기 (거리, x좌표, y좌표)
-  can_eat_fish_list.sort(key = lambda x: (x[0], x[1], x[2]))
-  dist, fish_x, fish_y = can_eat_fish_list[0]
-  
-  # 아기 상어의 위치 업데이트
-  baby_x, baby_y = fish_x, fish_y
-  space[fish_x][fish_y] = 0
-  eating_count += 1
-  answer += dist
-  
-  # 아기 상어의 크기 증가
-  if baby_size == eating_count:
-    baby_size += 1
-    eating_count = 0
+  while q:
+    dist, current_x, current_y = q.popleft()
     
-print(answer)
+    # 상하좌우로 이동가능
+    for dx, dy in [ (-1,0), (1,0), (0,-1), (0,1) ]:
+      nx, ny = current_x + dx, current_y + dy
+      
+      # 범위를 벗어나면
+      if nx < 0 or nx >= N or ny < 0 or ny >= N:
+        continue
+      
+      if not visited[nx][ny] and space[nx][ny] <= shark_size:
+        q.append((dist + 1, nx, ny))
+        visited[nx][ny] = True
+        
+        # 빈공간이 아니고 아기상어보다 크기가 작은 물고기만 섭취 가능
+        if 0 < space[nx][ny] < shark_size:
+          possible.append((dist + 1, nx, ny))
+  
+  return possible
+
+answer = 0
+eating_cnt = 0
+
+while (True):
+  current_fish_list = get_possible_fish_list(baby_x, baby_y, baby_size)
+
+  # 엄마 상어 부르기 
+  if len(current_fish_list) == 0:
+    print(answer)
+    exit(0)
+
+  # 조건에 맞게 정렬, 타겟 물고기 좌표 받아오기
+  current_fish_list.sort(key = lambda x: (x[0], x[1], x[2]))
+  cost, cx, cy = current_fish_list[0]
+
+  # 먹방 후 아기 상어 위치 변경 (1초 소요)
+  space[cx][cy] = 0
+  baby_x, baby_y = cx, cy
+  eating_cnt += 1
+  answer += cost
+
+  if baby_size == eating_cnt:
+    baby_size += 1
+    eating_cnt = 0
