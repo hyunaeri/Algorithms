8060e7f4cbec162ecd66f4521b2c6aed671febab
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-11-06 17:08:04 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,48 +1,55 @@
+# 백준 10026
+# 골드 5
+# https://www.acmicpc.net/problem/10026
+
+from collections import deque
 import sys
-sys.setrecursionlimit(1000000)
-input = sys.stdin.readline
+read = sys.stdin.readline
 
-n = int(input().rstrip())
-matrix = [list(input().rstrip()) for _ in range(n)]
-visited = [[False] * n for _ in range(n)]
+def solution(x, y, N, space, visited):
+  q = deque([(x, y)])
+  visited[x][y] = True
+  
+  while q:
+    x, y = q.popleft()
+    current_color = space[x][y]
+    
+    for dx, dy in [ (-1, 0), (1, 0), (0, -1), (0, 1) ]:
+      nx, ny = x + dx, y + dy
+      
+      if nx < 0 or nx >= N or ny < 0 or ny >= N:
+        continue
+      
+      next_color = space[nx][ny]
+      
+      if not visited[nx][ny] and current_color == next_color:
+        q.append((nx, ny))
+        visited[nx][ny] = True
 
-three_cnt, two_cnt = 0, 0
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
-def dfs(x,y):
-    #현재 색상 좌표를 방문해준다.
-    visited[x][y] = True
-    current_color = matrix[x][y]
-
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-        if (0 <= nx < n) and (0 <= ny < n):
-            #현재 좌표의 색상과 상하좌우 좌표에 있는 색상이 같으면 dfs로 넣어준다.
-            if visited[nx][ny]==False:
-                if matrix[nx][ny] == current_color:
-                    dfs(nx,ny)
-
-for i in range(n):
-    for j in range(n):
-        # 방문하지 않은 좌표이면 dfs로 넣어준다.
-        if visited[i][j]==False:
-            dfs(i,j)
-            three_cnt += 1
-
-#R을 G로 바꾸어준다. --> 적록색약은 같은 색으로 보기 때문에
-for i in range(n):
-    for j in range(n):
-        if matrix[i][j]=='R':
-            matrix[i][j]='G'
-
-visited = [[False] * n for _ in range(n)]
-
-for i in range(n):
-    for j in range(n):
-        if visited[i][j] == False:
-            dfs(i,j)
-            two_cnt += 1
-
-print(three_cnt,two_cnt)
+if __name__ == "__main__":
+  general = 0
+  disabled = 0
+  
+  # N * N 크기의 공간
+  N = int(read())
+  
+  # 그림 정보
+  paint = [ read().rstrip() for _ in range(N) ]
+  visited = [ [False] * N for _ in range(N) ]
+  
+  for i in range(N):
+    for j in range(N):
+      if not visited[i][j]:
+        solution(i, j, N, paint, visited)
+        general += 1
+  
+  paint_revise = [row.replace('G', 'R') for row in paint]
+  visited = [ [False] * N for _ in range(N) ]
+  
+  for i in range(N):
+    for j in range(N):
+      if not visited[i][j]:
+        solution(i, j, N, paint_revise, visited)
+        disabled += 1
+  
+  print(general, disabled)
