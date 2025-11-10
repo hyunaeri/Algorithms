167e28b2743dcf35b8031ba90e8f71f703a2562f
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-11-10 15:17:10 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,15 +1,29 @@
-# 백준 2839 설탕 배달
+# 백준 2839
+# 실버 4
+# https://www.acmicpc.net/problem/2839
+
 import sys
 read = sys.stdin.readline
 
-N = int(read().rstrip())
-dp = [ 10001 for _ in range(N+5) ]
-dp[3], dp[5] = 1, 1
-
-for i in range(6, N+1):
-    dp[i] = min(dp[i-3], dp[i-5]) + 1
+if __name__ == '__main__':
+  
+  # N kg 의 설탕
+  N = int(read())
+  
+  bag_5 = N // 5
+  bag_3 = 0
+  remain = N % 5
+  
+  while True:
+    # 남은 설탕이 3으로 나누어 떨어지면
+    if remain % 3 == 0:
+      bag_3 = remain // 3
+      print(bag_3 + bag_5)
+      exit(0)
     
-if dp[N] >= 10001:
-    print("-1")
-else:
-    print(dp[N])
+    if bag_5 == 0:
+      print(-1)
+      exit(0)
+      
+    remain += 5
+    bag_5 -= 1
