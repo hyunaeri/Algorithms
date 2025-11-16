296df068b477a5ab1c3cffd247ab4766736e27d4
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-11-16 20:47:25 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -5,59 +5,34 @@
 import sys
 read = sys.stdin.readline
 
-# dir (0: 가로, 1: 세로, 2: 대각선)
-def dfs(x, y, dir):
-  global N, answer, house_info
-  
-  # 목표 지점 도달
-  if x == (N - 1) and y == (N - 1):
-    answer += 1
-    return
-  
-  # 가로
-  if dir == 0:
-    # 회전 없이
-    if 0 <= x < N and 0 <= y + 1 < N and house_info[x][y + 1] == 0:
-      dfs(x, y + 1, 0)
-    
-    # 45도 회전 (3방향 체크), 대각선으로 전환
-    if 0 <= x + 1 < N and 0 <= y + 1 < N and house_info[x][y + 1] == 0 and house_info[x + 1][y + 1] == 0 and house_info[x + 1][y] == 0:
-      dfs(x + 1, y + 1, 2)
-  
-  # 세로
-  if dir == 1:
-    # 회전 없이
-    if 0 <= x + 1 < N and 0 <= y < N and house_info[x + 1][y] == 0:
-      dfs(x + 1, y, 1)
-      
-    # 45도 회전 (3방향 체크), 대각선으로 전환
-    if 0 <= x + 1 < N and 0 <= y + 1 < N and house_info[x][y + 1] == 0 and house_info[x + 1][y + 1] == 0 and house_info[x + 1][y] == 0:
-      dfs(x + 1, y + 1, 2)
-  
-  # 대각선
-  if dir == 2:
-    # 회전 없이 대각선 유지
-    if 0 <= x + 1 < N and 0 <= y + 1 < N and house_info[x][y + 1] == 0 and house_info[x + 1][y + 1] == 0 and house_info[x + 1][y] == 0:
-      dfs(x + 1, y + 1, 2)
-    
-    # 45도 회전, 가로
-    if 0 <= x < N and 0 <= y + 1 < N and house_info[x][y + 1] == 0:
-      dfs(x, y + 1, 0)
-    
-    # 45도 회전, 세로
-    if 0 <= x + 1 < N and 0 <= y < N and house_info[x + 1][y] == 0:
-      dfs(x + 1, y, 1)
-
-
 if __name__ == '__main__':
-  answer = 0
   
   # 집의 크기 N * N
   N = int(read())
   
-  # 집의 상태 (0 : 빈칸, 1 : 벽)
+  # 집의 상태 (0: 빈칸, 1: 벽)
   house_info = [ list(map(int, read().rstrip().split())) for _ in range(N) ]
   
-  dfs(0, 1, 0)
+  # dp[dir][x][y] (0: 가로, 1: 세로, 2: 대각선)
+  dp = [ [[0] * N for _ in range(N)] for _ in range(3) ]
+  dp[0][0][1] = 1
   
-  print(answer)
+  # 첫 행 가로 방향 DP 테이블 채우기
+  for i in range(2, N):
+    if house_info[0][i] == 0:
+      dp[0][0][i] = dp[0][0][i - 1]
+  
+  # 나머지 DP 테이블 채우기
+  for i in range(1, N):
+    for j in range(1, N):
+      # 대각선 파이프 놓기
+      if house_info[i][j] == 0 and house_info[i][j-1] == 0 and house_info[i-1][j] == 0:
+        dp[2][i][j] = dp[0][i-1][j-1] + dp[1][i-1][j-1] + dp[2][i-1][j-1]
+      
+      if house_info[i][j] == 0:
+        # 가로 파이프 놓기
+        dp[0][i][j] = dp[0][i][j-1] + dp[2][i][j-1]
+        # 세로 파이프 놓기
+        dp[1][i][j] = dp[1][i-1][j] + dp[2][i-1][j]
+        
+  print(dp[0][N-1][N-1] + dp[1][N-1][N-1] + dp[2][N-1][N-1])
