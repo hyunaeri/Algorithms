0d7d294fa9d83f67f1ff134253d96d141bafeca4
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2025-11-16 22:04:33 +0900

    [Silver IV] Title: 로프, Time: 100 ms, Memory: 40652 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -12,18 +12,16 @@ if __name__ == '__main__':
   
   # 각 로프 정보
   rope_info = [ int(read()) for _ in range(N) ]
-  rope_info.sort(key = lambda x: -x)
+  rope_info.sort(key = lambda x: x)
   
   # for rope in rope_info:
   #   print(rope)
   
-  # 최대 중량 계산
-  answer = 0
+  # 로프를 사용해서 들어올릴 수 있는 최대 중량
+  answer = []
   
-  # i번째 로프까지, 총 i+1개 사용
-  for i in range(N):
-      w = rope_info[i] * (i + 1)   # 현재까지 사용 가능한 로프의 최소값 × 개수
-      if w > answer:
-          answer = w
+  for rope in rope_info:
+    answer.append(rope * N)
+    N -= 1
   
-  print(answer)
+  print(max(answer))
