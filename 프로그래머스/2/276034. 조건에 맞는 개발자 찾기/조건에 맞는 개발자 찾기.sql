9778c219d41345201a0a864c0c315e23e3d107a9
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2026-02-20 19:23:12 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,6 @@
+select distinct d.id, d.email, d.first_name, d.last_name
+from developers d
+join skillcodes s 
+    on (s.code & d.skill_code) = s.code
+where s.name in ('Python', 'C#')
+order by d.id asc;
