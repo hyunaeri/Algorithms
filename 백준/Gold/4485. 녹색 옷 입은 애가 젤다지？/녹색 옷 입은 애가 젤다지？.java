dec496b884e61d3e6c364eeb276456f0f92f1248
Seohyun Jeong <75007741+hyunaeri@users.noreply.github.com>
2026-03-02 17:21:44 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 192 ms, Memory: 18808 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -7,7 +7,7 @@ public class Main {
 
     static int N, answer;
     static int[][] cave;
-    static int[][] visited;
+    static int[][] dist;
 
     static boolean isInside(int x, int y) {
         return 0 <= x && x < N && 0 <= y && y < N;
@@ -21,13 +21,24 @@ public class Main {
                     return Integer.compare(e1[0], e2[0]);
                 }
         );
-        pq.offer(new int[] { cave[0][0], 0, 0 });
-        visited[0][0] = cave[0][0];
+        dist[0][0] = cave[0][0];
+        pq.offer(new int[] { dist[0][0], 0, 0 });
 
         while (!pq.isEmpty()) {
+
+            // 우선 순위 큐에서 꺼낸 노드는 해당 노드까지의 최단 거리가 확정된 상태
             int[] cur = pq.poll();
+            int cost = cur[0];
             int x  = cur[1];
             int y = cur[2];
+
+            // 우선 순위 큐에 같은 좌표가 여러 번 들어올 수 있는데, 이미 더 좋은 거리로 갱신 됐다면 무시
+            if (cost != dist[x][y]) continue;
+
+            if (x == N - 1 && y == N - 1) {
+                answer = cost;
+                return;
+            }
 
             for (int i = 0; i < 4; i++) {
                 int nx = x + directions[i][0];
@@ -36,12 +47,12 @@ public class Main {
                 if (!isInside(nx, ny)) continue;
 
                 // 다음 좌표까지 드는 비용
-                int nextCost = visited[x][y] + cave[nx][ny];
+                int nextCost = cost + cave[nx][ny];
 
                 // 다음 좌표까지 더 짧은 비용으로 갈 수 있다면
-                if (visited[nx][ny] > nextCost) {
-                    pq.offer(new int[] { cave[nx][ny], nx, ny });
-                    visited[nx][ny] = nextCost;
+                if (dist[nx][ny] > nextCost) {
+                    dist[nx][ny] = nextCost;
+                    pq.offer(new int[] { nextCost, nx, ny });
                 }
             }
         }
@@ -60,9 +71,9 @@ public class Main {
         while (N != 0) {
             answer  = Integer.MAX_VALUE;
             cave = new int[N][N];
-            visited = new int[N][N];
+            dist = new int[N][N];
 
-            for (int[] row: visited) {
+            for (int[] row: dist) {
                 Arrays.fill(row, Integer.MAX_VALUE);
             }
 
@@ -77,8 +88,6 @@ public class Main {
 
             // 최소 비용 탐색
             solution();
-
-            answer = visited[N - 1][N - 1];
 
             sb.append("Problem ").append(tc++).append(": ").append(answer).append("\n");
 
